f1202f103b7c0e19f5694cb4438f671c6b0799f3
dover20 <doverlin17@gmail.com>
2020-03-19 01:50:54 -0400

    second commit

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ async function fetchAPI() {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 15,
         minZoom: 2,
-        //id: 'baconator45/ck7y8p0pf0mrq1jp119zpvxgb',
         id: 'baconator45/ck7y95u8o0n5u1iqdpws3vyyo',
         tileSize: 512,
         zoomOffset: -1,
